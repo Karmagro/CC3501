e5b633a1f9df7780b4af7fe8947aee674bb6eb25
Karmagro <carlosmgalvezr@gmail.com>
2023-04-05 22:10:24 -0400

    Añadiendo funcionalidades extra

--- Proyecto.py
+++ Proyecto.py
@@ -1,4 +1,6 @@
 import pyglet
+import numpy as np
+import noise
 from OpenGL.GL import *
 
 from math import cos, sin
@@ -9,6 +11,14 @@ from grafica import easy_shaders as es
 from grafica import transformations as tr
 from grafica.assets_path import getAssetPath
 from shapes_utils import HighLevelGPUShape, createGPUShape
+
+def generate_noise(width, height, time, octaves=1, persistence=0.5, lacunarity=2.0):
+    noise_matrix = [[0 for y in range(height)] for x in range(width)]
+    for x in range(width):
+        for y in range(height):
+            noise_matrix[x][y] = noise.pnoise3(x / 20.0, y / 20.0, time, octaves=octaves, persistence=persistence, lacunarity=lacunarity)
+    return noise_matrix
+
 class Shape:
     def __init__(self, vertices, indices):
         self.vertices = vertices
@@ -35,8 +45,6 @@ class Controller(pyglet.window.Window):
 
 controller = Controller(width=1280, height=720)
 
-glClearColor(0.6, 1, 1, 1)
-
 pipeline = es.SimpleTransformShaderProgram()
 controller.pipeline = pipeline
 glUseProgram(pipeline.shaderProgram)
@@ -54,16 +62,16 @@ def createSpecificTriangle(x1,y1,z1,x2,y2,z2,x3,y3,z3,r1, g1, b1,r2, g2, b2,r3, 
 
 def createSpecificQuad(x1,y1,z1,x2,y2,z2,x3,y3,z3,x4,y4,z4,r1, g1, b1,r2,g2,b2,r3,g3,b3,r4,g4,b4):
 
-    # Defining locations and colors for each vertex of the shape    
+    
     vertices = [
-    #   positions        colors
+    
         x1, y1, z1,  r1, g1, b1,
         x2, y2, z2,  r2, g2, b2,
         x3, y3, z3,  r3, g3, b3,
         x4, y4, z4,  r4, g4, b4]
 
-    # Defining connections among vertices
-    # We have a triangle every 3 indices specified
+    
+    
     indices = [
          0, 1, 2,
          2, 3, 0]
@@ -79,13 +87,13 @@ alaIzqSup = HighLevelGPUShape(pipeline, createSpecificTriangle(0.0,0.0,0.0,  -0.
 
 alaIzqInf = HighLevelGPUShape(pipeline, createSpecificTriangle(0.0,0.0,0.0,  -0.193,-0.35,0.0,  -0.3,0.0,0.0,  1, 0.3, 0.1,  1, 0.6, 0.0,  1, 0.4, 0.1))
 
-#cuerpo = HighLevelGPUShape(pipeline, createSpecificQuad(-0.05,0.2,0.0,  0.05,0.2,0.0,  0.05,-0.2,0.0,  -0.05,-0.2,0.0,  0.6,0.3,0.0,  0.6,0.3,0.0,  0.6,0.3,0.0,  0.6,0.3,0.0))
 
-cuerpo = HighLevelGPUShape(pipeline, createSpecificQuad(-0.03,0.2,0.0,  0.03,0.2,0.0,  0.03,-0.25,0.0,  -0.03,-0.25,0.0,  0,0,0,  0,0,0,  0,0,0, 0,0,0))
 
-antenaIzq= HighLevelGPUShape(pipeline,createSpecificQuad(-0.095,0.275,0.0,  -0.085,0.285,0.0,  -0.02,0.2,0.0,  -0.03,0.19,0.0,  0,0,0,  0,0,0,  0,0,0, 0,0,0))
+cuerpo = HighLevelGPUShape(pipeline, createSpecificQuad(-0.03,0.2,0.0,  0.03,0.2,0.0,  0.03,-0.25,0.0,  -0.03,-0.25,0.0,  1.0, 1.0, 0.5,  0.7, 1.0, 0.9,  0.6, 0.8, 1.0, 1.0, 0.8, 0.6))
 
-antenaDer= HighLevelGPUShape(pipeline,createSpecificQuad(0.095,0.275,0.0,  0.085,0.285,0.0,  0.02,0.2,0.0,  0.03,0.19,0.0,  0,0,0,  0,0,0,  0,0,0, 0,0,0))
+antenaIzq= HighLevelGPUShape(pipeline,createSpecificQuad(-0.095,0.275,0.0,  -0.085,0.285,0.0,  -0.02,0.2,0.0,  -0.03,0.19,0.0,  0.4, 0.2, 0.5,  0.5, 0.3, 0.6,  0.6, 0.4, 0.7, 0.7, 0.5, 0.9))
+
+antenaDer= HighLevelGPUShape(pipeline,createSpecificQuad(0.095,0.275,0.0,  0.085,0.285,0.0,  0.02,0.2,0.0,  0.03,0.19,0.0,  0.4, 0.2, 0.5,  0.5, 0.3, 0.6,  0.6, 0.4, 0.7, 0.7, 0.5, 0.9))
 
 ojoIzq= HighLevelGPUShape(pipeline,createSpecificQuad(-0.02,0.19,0.0,  -0.01,0.19,0.0,  -0.01,0.16,0.0,  -0.02,0.16,0.0,  1,0.1,0.7,  1,0.1,0.7,  1,0.1,0.7, 1,0.1,0.7))
 
@@ -94,11 +102,11 @@ ojoDer= HighLevelGPUShape(pipeline,createSpecificQuad(0.01,0.19,0.0, 0.02,0.19,0
 mariposa= [alaDerSup,alaDerInf,alaIzqSup,alaIzqInf,cuerpo,antenaDer,antenaIzq,ojoIzq,ojoDer]
 
 def draw_moving_mariposa(controller: Controller):
-
+    
     for parte in mariposa:
         if parte==alaDerSup or parte==alaDerInf or parte==alaIzqInf or parte==alaIzqSup:
             parte._transform = tr.matmul([tr.translate(controller.mariposaPosX, controller.mariposaPosY, 0),
-                                        tr.uniformScale(0.3+controller.total_time%0.7)])
+                                        tr.uniformScale(0.45 + 0.35 * np.sin(2 * np.pi * controller.total_time))])
         else:
             parte._transform = tr.matmul([tr.translate(controller.mariposaPosX, controller.mariposaPosY, 0),
                                         tr.uniformScale(1)])
@@ -106,26 +114,26 @@ def draw_moving_mariposa(controller: Controller):
 
 
 def draw_enjambre(controller: Controller):
+
     i=0
-    rutina = [[0.4*cos(controller.total_time),-0.4*sin(controller.total_time),0.0],
-              [-0.9*cos(controller.total_time),0.9*sin(controller.total_time),0.0],
-              [0.5*cos(controller.total_time),0.5*sin(controller.total_time),0.0],
-              [-0.7*cos(controller.total_time),-0.7*sin(controller.total_time),0.0],
-              [1*cos(controller.total_time),-1*sin(controller.total_time),0.0]]
-    while i<5:
+    posicion_noise = generate_noise(10, 2, controller.total_time*0.0005, octaves=10, persistence=2, lacunarity=2)
+    
+    while i<10:
         for parte in mariposa:
                 seed(i)
+                x= posicion_noise[i][0] * 5.0
+                y= posicion_noise[i][1] * 5.0
                 if parte==alaDerSup or parte==alaDerInf or parte==alaIzqInf or parte==alaIzqSup:
-                    parte._transform = tr.identity()  # Make the transform neutral again
+                    parte._transform = tr.identity() 
                     parte.rotation = tr.rotationZ(controller.total_time*random()*2)
-                    parte.translation = tr.translate(rutina[i][0],rutina[i][1],rutina[i][2])
-                    parte.scale = tr.uniformScale(0.1 + controller.total_time%0.2)
+                    parte.translation = tr.translate(x, y, 0.0)
+                    parte.scale = tr.uniformScale(0.15 + 0.15 * np.sin(2 * np.pi * controller.total_time))
                     parte.draw(controller.pipeline)
 
                 else:
-                    parte._transform = tr.identity()  # Make the transform neutral again
+                    parte._transform = tr.identity()  
                     parte.rotation = tr.rotationZ(controller.total_time*random()*2)
-                    parte.translation = tr.translate(rutina[i][0],rutina[i][1],rutina[i][2])
+                    parte.translation = tr.translate(x, y, 0.0)
                     parte.scale = tr.uniformScale(0.3)
                     parte.draw(controller.pipeline)            
         i+=1
@@ -171,23 +179,24 @@ def update(dt, controller):
     controller.total_time += dt
     controller.mariposaPosX += controller.mariposaSpeedX * dt
     controller.mariposaPosY += controller.mariposaSpeedY * dt
+    glClearColor(0.529 - 0.479*sin(controller.total_time*0.05), 0.808 - 0.758*sin(controller.total_time*0.05), 0.922 - 0.772*sin(controller.total_time*0.05),1)
 
 
 @controller.event
 def on_draw():
     controller.clear()
 
-    # Si el controller está en modo fillPolygon, dibuja polígonos. Si no, líneas.
+    
     if controller.fillPolygon:
         glPolygonMode(GL_FRONT_AND_BACK, GL_FILL)
     else:
         glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
     
-    # Dibuja las figuras
+    
     draw_moving_mariposa(controller)
     draw_enjambre(controller)    
 
-# Try to call this function 60 times per second
+
 pyglet.clock.schedule(update, controller)
-# Se ejecuta la aplicación
+
 pyglet.app.run()
--- purebas.py
+++ purebas.py
@@ -1,7 +1,74 @@
-lista = ["goma","mascar"]
+import pyglet
+from pyglet.gl import *
+import numpy as np
+import math
 
-for name in lista:
-    print(name)
-    
-for i in 5:
-    print("Hola")
+window = pyglet.window.Window(640, 480, visible=True)
+
+# Shader source code for the vertex and fragment shaders
+vertex_shader_source = """
+#version 330
+in vec2 position;
+void main() {
+    gl_Position = vec4(position, 0.0, 1.0);
+}
+"""
+
+fragment_shader_source = """
+#version 330
+out vec4 out_color;
+void main() {
+    out_color = vec4(1.0, 0.0, 0.0, 1.0);
+}
+"""
+
+# Create the shader program
+shader_program = glCreateProgram()
+
+# Create the vertex shader, compile it, and attach it to the shader program
+vertex_shader = glCreateShader(GL_VERTEX_SHADER)
+glShaderSource(vertex_shader, vertex_shader_source)
+glCompileShader(vertex_shader)
+glAttachShader(shader_program, vertex_shader)
+
+# Create the fragment shader, compile it, and attach it to the shader program
+fragment_shader = glCreateShader(GL_FRAGMENT_SHADER)
+glShaderSource(fragment_shader, fragment_shader_source)
+glCompileShader(fragment_shader)
+glAttachShader(shader_program, fragment_shader)
+
+# Link the shader program
+glLinkProgram(shader_program)
+
+# Create a vertex buffer for the circle
+num_segments = 64
+vertices = []
+for i in range(num_segments):
+    angle = 2 * math.pi * i / num_segments
+    x = math.cos(angle)
+    y = math.sin(angle)
+    vertices += [x, y]
+vbo = (GLfloat * len(vertices))(*vertices)
+vbo_size = len(vertices) * 4
+
+# Create a vertex array object for the circle
+vao = GLuint(0)
+glGenVertexArrays(1, vao)
+glBindVertexArray(vao)
+vbo_handle = GLuint(0)
+glGenBuffers(1, vbo_handle)
+glBindBuffer(GL_ARRAY_BUFFER, vbo_handle)
+glBufferData(GL_ARRAY_BUFFER, vbo_size, vbo, GL_STATIC_DRAW)
+glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, None)
+glEnableVertexAttribArray(0)
+
+@window.event
+def on_draw():
+    glClear(GL_COLOR_BUFFER_BIT)
+
+    # Use the shader program to draw the circle
+    glUseProgram(shader_program)
+    glBindVertexArray(vao)
+    glDrawArrays(GL_TRIANGLE_FAN, 0, num_segments)
+
+pyglet.app.run()
